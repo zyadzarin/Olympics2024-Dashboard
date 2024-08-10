c05b9effd76b7a9684e2d30067d3b8fa1bbffc4a
Michelle-Lohwt <michellelohwt0799@gmail.com>
2024-08-10 20:41:26 +0800

    feat(api): Add CountryMedalsHistory api

--- django/apps/olympics_games/urls.py
+++ django/apps/olympics_games/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from . import views
-from .views import MedallistsStatsView, CountryMedalsView
+from .views import MedallistsStatsView, CountryMedalsView, CountryMedalsHistoryView
 
 urlpatterns = [
   path('general_stats/', MedallistsStatsView.as_view(), name='medallists-stats'),
   path('country_medals/', CountryMedalsView.as_view(), name='country-medals'),
   path('events', views.sport_list, name='events'),
+  
+  path('country_medals_history/', CountryMedalsHistoryView.as_view(), name='country-medals-history'),
 ]
 
 
--- django/apps/olympics_games/views.py
+++ django/apps/olympics_games/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Count
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Events, Athletes, Medals, Athletes, Medallists, MedalsTotal
-from django.db.models import Q
-from .serializers import MedallitstsStatsSerializer, CountryMedalsSerializer
+from .models import Events, Medals, Athletes, Medallists, MedalsTotal, MedalsTally
+from .serializers import MedallitstsStatsSerializer, CountryMedalsSerializer, CountryMedalsHistorySerializer
 
 # Create your views here.
 class MedallistsStatsView(APIView):
@@ -27,6 +25,36 @@ class MedallistsStatsView(APIView):
 
     serializer = MedallitstsStatsSerializer(data)
     return Response(serializer.data)
+  
+# class CountryMedalsView(APIView):
+#   def get(self, request):
+#     # Get top 5 countries by total medals
+#     top_5_medals = MedalsTotal.objects.order_by('-total')[:5]
+
+#     # Get athlete counts for these top 5 countries
+#     country_codes = [medal.country_code for medal in top_5_medals]
+#     athlete_counts = Athletes.objects.filter(country_code__in=country_codes)\
+#                       .values('country_code')\
+#                       .annotate(athletes_num=Count('code'))
+
+#     # Combine the data
+#     combined_data = []
+#     for medal in top_5_medals:
+#       athlete_count = next((item for item in athlete_counts if item['country_code'] == medal.country_code), None)
+#       country = Athletes.objects.filter(country_code=medal.country_code).values_list('country', flat=True).first() or ''
+#       country_data = {
+#         'country_code': medal.country_code,
+#         'country': country,
+#         'gold': medal.gold_medal or 0,
+#         'silver': medal.silver_medal or 0,
+#         'bronze': medal.bronze_medal or 0,
+#         'total': medal.total or 0,
+#         'athletes_num': athlete_count['athletes_num'] if athlete_count else 0
+#       }
+#       combined_data.append(country_data)
+
+#     serializer = CountryMedalsSerializer(combined_data, many=True)
+#     return Response(serializer.data)
 
 class CountryMedalsView(APIView):
   def get(self, request):
@@ -112,3 +140,34 @@ def sport_list(request):
 
   # Return the data as JSON response
   return JsonResponse({'sports': sport_list}, encoder=DjangoJSONEncoder)
+
+class CountryMedalsHistoryView(APIView):
+  def get(self, request):
+    # Get all countries from MedalsTotal
+    countries = MedalsTotal.objects.all()
+
+    combined_data = []
+    for country in countries:
+      # Get past years' data
+      past_medals = MedalsTally.objects.filter(country_noc=country.country_code)\
+        .values('year', 'gold', 'silver', 'bronze', 'total')\
+        .order_by('year')
+
+      # Add 2024 data
+      medals_history = list(past_medals) + [{
+        'year': 2024,
+        'gold': country.gold_medal or 0,
+        'silver': country.silver_medal or 0,
+        'bronze': country.bronze_medal or 0,
+        'total': country.total or 0
+      }]
+
+      country_data = {
+        'country_code': country.country_code,
+        'country': MedalsTally.objects.filter(country_noc=country.country_code).values_list('country', flat=True).first() or '',
+        'medals_history': medals_history
+      }
+      combined_data.append(country_data)
+
+    serializer = CountryMedalsHistorySerializer(combined_data, many=True)
+    return Response(serializer.data)
